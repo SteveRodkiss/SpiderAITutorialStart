6557a46dc654601c56036fed2a8a7443d4ff68cc
SRCoder <SteveRodkiss@users.noreply.github.com>
2019-10-12 11:56:35 +1300

    end of part 3- created states in the enemy.cs file

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -6,6 +6,10 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    public enum AIState { idle,moving,attacking};
+
+    public AIState aiState = AIState.idle;
+
     NavMeshAgent nav;
     Transform player;
 
@@ -19,6 +23,34 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        nav.SetDestination(player.position);
+        float dist = Vector3.Distance(player.position, transform.position);
+        switch (aiState)
+        {
+            case AIState.idle:
+                //what we do in idle- switch states
+                if (dist < 10f)
+                {
+                    aiState = AIState.moving;
+                    //set the targer destination
+                    //nav.SetDestination(player.position);
+                }
+                break;
+            case AIState.moving:
+                //what we do switch state
+                nav.SetDestination(player.position);
+                if (dist > 10f)
+                {
+                    nav.SetDestination(transform.position);
+                    aiState = AIState.idle;
+                }
+                break;
+            case AIState.attacking:
+                break;
+            default:
+                break;
+        }
+
+
+        
     }
 }
