3ab518a7fc8c2bfacc02b57bda09793375054217
SRCoder <SteveRodkiss@users.noreply.github.com>
2019-10-12 11:15:36 +1300

    end of tutorial 1

---
+++ Assets/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+
+    NavMeshAgent nav;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nav.SetDestination(Vector3.zero);
+    }
+}
